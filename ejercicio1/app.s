de00abb2ca63c03fa7ee3b2196142bdcad196855
Emma Barraud <emma.barraud@mi.unc.edu.ar>
2025-05-23 19:29:03 -0300

    fondo negro-linea blanca q separa el upside down del mundo

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -19,13 +19,49 @@ main:
 	mov x2, SCREEN_HEIGH         // Y Size
 loop1:
 	mov x1, SCREEN_WIDTH         // X Size
-loop0:
-	stur w10,[x0]  // Colorear el pixel N
+	// ------------------- PINTAR FONDO NEGRO ----------------------
+
+	// Color negro = 0xFF000000
+	movz x10, 0x0000, lsl 0
+	movk x10, 0x0000, lsl 16
+	movk x10, 0x0000, lsl 32
+	movk x10, 0xFF00, lsl 32     //
+
+	mov x2, SCREEN_HEIGH         // Y Size
+loop_fill:
+	mov x1, SCREEN_WIDTH         // X Size
+loop_fill_x:
+	stur w10,[x0]  // Colorear el pixel negro
 	add x0,x0,4	   // Siguiente pixel
 	sub x1,x1,1	   // Decrementar contador X
-	cbnz x1,loop0  // Si no terminó la fila, salto
+	cbnz x1,loop_fill_x  // Si no terminó la fila, salto
 	sub x2,x2,1	   // Decrementar contador Y
-	cbnz x2,loop1  // Si no es la última fila, salto
+	cbnz x2,loop_fill  // Si no es la última fila, salto
+
+	// ------------------- PINTAR LINEA BLANCA ----------------------
+
+	// Color blanco = 0xFFFFFFFF
+	movz x11, 0xFFFF, lsl 16
+	movk x11, 0xFFFF, lsl 0
+
+	// fila donde va la linea
+	mov x12, SCREEN_HEIGH
+	lsr x12, x12, 1          // x12 = SCREEN_HEIGH / 2 = 240
+
+	// índice base de la fila: (240 * 640) * 4 bytes
+	mov x13, SCREEN_WIDTH
+	mul x13, x13, x12        // x13 = 240 * 640 (cantidad de pixels previos
+	lsl x13, x13, 2          // *4 bytes por pixel
+
+	add x14, x20, x13        // dirección base del pixel de la fila 240
+
+	mov x15, SCREEN_WIDTH    // ancho de la pantalla para pintar la línea
+
+line_loop:
+	str w11, [x14], #4       //  pixel blanco y avanzo 4 bytes osea 1 pixel
+	sub x15, x15, 1
+	cbnz x15, line_loop
+
 
 	// Ejemplo de uso de gpios
 	mov x9, GPIO_BASE
